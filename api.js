b91d172b19ba3100578ff37f8e93a03490682b45
Rythelle C. Pereira <rythelle20@hotmail.com>
2021-12-14 11:05:48 -0300

    Version-1.0

--- api.js
+++ api.js
@@ -1,12 +1,61 @@
 const axios = require("axios");
 const querystring = require("query-string");
+const crypto = require("crypto");
+
+const apiKey = process.env.API_KEY;
+const apiSecret = process.env.SECRET_KEY;
+const apiUrl = process.env.API_URL;
+
+async function privateCall(path, data = {}, method = "GET") {
+  const timestamp = Date.now();
+
+  const recvWindow = 60000;
+
+  const signature = crypto
+    .createHmac("sha256", apiSecret)
+    .update(`${querystring.stringify({ ...data, timestamp, recvWindow })}`)
+    .digest("hex");
+
+  const newData = { ...data, timestamp, signature, recvWindow };
+  const qs = `?${querystring.stringify(newData)}`;
+
+  try {
+    const result = await axios({
+      method,
+      url: `${apiUrl}${path}${qs}`,
+      headers: { "X-MBX-APIKEY": apiKey },
+    });
+    return result.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function accountInfo() {
+  return privateCall("/v3/account");
+}
+
+async function newOrder(
+  symbol,
+  quantity,
+  price,
+  side = "BUY",
+  type = "MARKET"
+) {
+  const data = { symbol, side, type, quantity };
+
+  if (price) data.price = price;
+  if (type === "LIMIT") data.timeInForce = "GTC";
+
+  return privateCall("/v3/order", data, "POST");
+}
 
 async function publicCall(path, data, method = "GET") {
   try {
     const qs = data ? `?${querystring.stringify(data)}` : "";
     const result = await axios({
       method,
-      url: `${process.env.API_URL}${path}${qs}`,
+      url: `${apiUrl}${path}${qs}`,
     });
 
     return result.data;
@@ -23,4 +72,8 @@ async function depth(symbol = "BTCBRL", limit = 5) {
   return await publicCall("/v3/depth", { symbol, limit });
 }
 
-module.exports = { time, depth };
+async function exchangeInfo() {
+  return await publicCall("/v3/exchangeInfo");
+}
+
+module.exports = { time, depth, exchangeInfo, accountInfo, newOrder };
--- index.js
+++ index.js
@@ -1,19 +1,63 @@
 const api = require("./api");
 
+const symbol = process.env.SYMBOL;
+const profitability = parseFloat(process.env.PROFITABILITY);
+const coin = process.env.COIN;
+const priceBuy = process.env.PRICE_BUY;
+const priceSell = process.env.PRICE_SELL;
+
 setInterval(async () => {
-  const result = await api.depth();
+  let buy = 0;
+  let sell = 0;
 
-  console.log(`Highest Buy: ${result.bids[0][0]}`);
-  console.log(`Lowest Sell: ${result.asks[0][0]}`);
+  const result = await api.depth(symbol);
+  if (result.bids && result.bids.length) {
+    console.log(`Highest Buy: ${result.bids[0][0]}`);
+    buy = parseFloat(result.bids[0][0]);
+  }
+  if (result.asks && result.asks.length) {
+    console.log(`Lowest Sell: ${result.asks[0][0]}`);
+    sell = parseFloat(result.asks[0][0]);
+    console.log(sell);
+  }
 
-  const buy = parseInt(result.bids[0][0]);
-  const sell = parseInt(result.asks[0][0]);
-
-  if (sell < 250000) {
+  if (sell && sell < priceBuy) {
     console.log("Hora de comprar!");
-  } else if (buy > 270000) {
+
+    const account = await api.accountInfo();
+    const coins = account.balances.filter(
+      (b) => symbol.indexOf(b.asset) !== -1
+    );
+    console.log("Valores atuais na carteira:");
+    console.log(coins);
+
+    console.log("Verificando se possui fundos");
+    const walletCoin = parseFloat(coins.find((c) => c.asset === coin).free);
+    console.log(walletCoin);
+    if (sell <= walletCoin) {
+      console.log("Realizando a compra");
+
+      const buyOrder = await api.newOrder(symbol, 1);
+
+      console.log(`Order ID: ${buyOrder.orderId}`);
+      console.log(`Status: ${buyOrder.status}`);
+
+      if (buyOrder.status === "FILLED") {
+        console.log("Posicionando venda futura...");
+        const price = parseFloat(sell * profitability).toFixed(8);
+
+        console.log(`Vendendo por: (${sell}) (${profitability})`);
+        const sellOrder = await api.newOrder(symbol, 1, price, "SELL", "LIMIT");
+
+        console.log(`Order ID: ${sellOrder.orderId}`);
+        console.log(`Status: ${sellOrder.status}`);
+      }
+    }
+  } else if (buy && buy > priceSell) {
     console.log("Hora de vender!");
   } else {
     console.log("Esperando...");
   }
+
+  // console.log(await api.exchangeInfo());
 }, process.env.CRAWLER_INTERVAL);
